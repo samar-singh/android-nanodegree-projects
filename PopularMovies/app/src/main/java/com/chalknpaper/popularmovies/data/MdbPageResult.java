ec0ecf773925ce916bee48b4d0efe12e1c4eaf48
Samar_Singh <bills23@gmail.com>
2017-08-16 08:07:39 +0530

    Using GSON , RxAndroid and Observer for data access

--- PopularMovies/app/src/main/java/com/chalknpaper/popularmovies/data/MdbPageResult.java
+++ PopularMovies/app/src/main/java/com/chalknpaper/popularmovies/data/MdbPageResult.java
@@ -1,86 +1,56 @@
+
 package com.chalknpaper.popularmovies.data;
 
-        import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-        "page",
-        "total_results",
-        "total_pages",
-        "mdbMovieResults"
-})
 public class MdbPageResult {
 
-    @JsonProperty("page")
+    @SerializedName("page")
+    @Expose
     private Integer page;
-    @JsonProperty("total_results")
+    @SerializedName("total_results")
+    @Expose
     private Integer totalResults;
-    @JsonProperty("total_pages")
+    @SerializedName("total_pages")
+    @Expose
     private Integer totalPages;
-    @JsonProperty("mdbMovieResults")
-    private List<MdbMovieResult> mdbMovieResults = null;
-    @JsonIgnore
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    @SerializedName("results")
+    @Expose
+    private List<Result> results = null;
 
-    @JsonProperty("page")
     public Integer getPage() {
         return page;
     }
 
-    @JsonProperty("page")
     public void setPage(Integer page) {
         this.page = page;
     }
 
-    @JsonProperty("total_results")
     public Integer getTotalResults() {
         return totalResults;
     }
 
-    @JsonProperty("total_results")
     public void setTotalResults(Integer totalResults) {
         this.totalResults = totalResults;
     }
 
-    @JsonProperty("total_pages")
     public Integer getTotalPages() {
         return totalPages;
     }
 
-    @JsonProperty("total_pages")
     public void setTotalPages(Integer totalPages) {
         this.totalPages = totalPages;
     }
 
-    @JsonProperty("mdbMovieResults")
-    public List<MdbMovieResult> getMdbMovieResults() {
-        return mdbMovieResults;
+    public List<Result> getResults() {
+        return results;
     }
 
-    @JsonProperty("mdbMovieResults")
-    public void setMdbMovieResults(List<MdbMovieResult> mdbMovieResults) {
-        this.mdbMovieResults = mdbMovieResults;
-    }
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
+    public void setResults(List<Result> results) {
+        this.results = results;
     }
 
 }
-
-
